f97493a704c85663af12214d71741c7421b0b9d6
rubwangb <ruben.wangberg@finn.no>
2013-12-27 21:32:13 +0100

    Added junk

---
+++ Kryss og Bolle/HelloWorldViewController.h
@@ -0,0 +1,13 @@
+//
+//  HelloWorldViewController.h
+//  Kryss og Bolle
+//
+//  Created by Wangberg, Ruben on 27.12.13.
+//  Copyright (c) 2013 Wangum. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HelloWorldViewController : UIViewController
+
+@end
---
+++ Kryss og Bolle/HelloWorldViewController.m
@@ -0,0 +1,38 @@
+//
+//  HelloWorldViewController.m
+//  Kryss og Bolle
+//
+//  Created by Wangberg, Ruben on 27.12.13.
+//  Copyright (c) 2013 Wangum. All rights reserved.
+//
+
+#import "HelloWorldViewController.h"
+
+@interface HelloWorldViewController ()
+
+@end
+
+@implementation HelloWorldViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
