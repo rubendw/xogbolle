4847173d1e06ac6242c3e3c9e5efd5185757ec90
rubwangb <ruben.wangberg@finn.no>
2013-12-27 20:41:25 +0100

    Initial Commit

---
+++ Kryss og Bolle/HelloWorldAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HelloWorldAppDelegate.h
+//  Kryss og Bolle
+//
+//  Created by Wangberg, Ruben on 27.12.13.
+//  Copyright (c) 2013 Wangum. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HelloWorldAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Kryss og Bolle/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Kryss og Bolle
+//
+//  Created by Wangberg, Ruben on 27.12.13.
+//  Copyright (c) 2013 Wangum. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "HelloWorldAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([HelloWorldAppDelegate class]));
+    }
+}
